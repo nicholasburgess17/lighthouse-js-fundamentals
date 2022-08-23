0aca9a9f68e7850deed2cf4fa51e5490c12767c1
Nicholas Burgess <Nicholas.Burgess13@gmail.comgit config --global user.name nicholas>
2022-08-23 07:21:26 +0000

    packing-list.js

---
+++ packing-list.js
@@ -0,0 +1,6 @@
+const packingList = ["bowls" , "plates" , "pots" , "pans" , "utensils" , "glasses" , "cups" , "cooking stuff"]
+
+console.log("Kitchen stuff to pack");
+for (let i = 0; i < packingList.length; i++) {
+  console.log(packingList[i]);
+}
